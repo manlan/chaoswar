29aca584f52585bf4681c21a7f6a3e69decd848f
Mac <mac@MacmatoMacBook.local>
2011-09-04 21:21:47 +0800

    Initial Commit

---
+++ chaoswar/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  chaoswar
+//
+//  Created by Mac on 11-9-4.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
